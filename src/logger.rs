4ed6781ef055e27afe78a837bdab54cdaecb55dd
Pat O'Connor <patoconnor43@gmail.com>
2020-11-29 15:54:11 -0700

    Refactor to separate modules

---
+++ src/logger.rs
@@ -0,0 +1,16 @@
+pub fn init() {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply()
+        .expect("Couldn't set up logger");
+}
