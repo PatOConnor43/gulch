ed26d58f1c46670e2ba52b5f7dc20fa0cb53edc5
Pat O'Connor <patoconnor43@gmail.com>
2020-11-29 18:36:48 -0700

    Parse query into struct

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,7 @@
+use rocket::{
+    http::RawStr,
+    request::FromParam,
+};
 use rocket_contrib::json::Json;
 use serde::{
     Deserialize,
@@ -5,14 +9,23 @@ use serde::{
 };
 
 #[derive(Deserialize, Debug)]
-pub struct SearchRequest {
-    query: String,
+pub struct SearchRequest<'a> {
+    query: &'a str,
+}
+impl<'a> FromParam<'a> for SearchRequest<'a> {
+    type Error = &'a RawStr;
+
+    fn from_param(param: &'a rocket::http::RawStr) -> Result<Self, Self::Error> {
+        Ok(SearchRequest {
+            query: param.as_str(),
+        })
+    }
 }
 
 #[derive(Serialize, Debug)]
 pub struct SearchResponse {}
 
-#[get("/search", format = "json")]
-pub fn search() -> Json<SearchResponse> {
+#[get("/search/<query>", format = "json")]
+pub fn search(query: SearchRequest) -> Json<SearchResponse> {
     Json(SearchResponse {})
 }
